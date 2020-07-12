ca867eab4fa884afaf03c380ec2220dc5114a9fc
A.Chubatyi <artem.chubatyi@rubyplay.com>
2020-07-12 13:17:31 +0300

    Updates

---
+++ precache-manifest.6336953a0d082b9ffa4dd86f82902293.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f68cf25f63fdab9b74ed0ade991bd5a",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "3db0baaf68fe423cfff5",
+    "url": "/website/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "cb55874bf17bc50fec31",
+    "url": "/website/static/css/main.b1f7778b.chunk.css"
+  },
+  {
+    "revision": "3db0baaf68fe423cfff5",
+    "url": "/website/static/js/2.230dfff7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.230dfff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb55874bf17bc50fec31",
+    "url": "/website/static/js/main.5f66a978.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "120160eea8d954c6e4c9e572397f054f",
+    "url": "/website/static/media/paper_texture_11.120160ee.png"
+  },
+  {
+    "revision": "d86bf2e5ad650ae7dc3a0add3d3deaa7",
+    "url": "/website/static/media/technology.d86bf2e5.d86bf2e5.png"
+  }
+]);
